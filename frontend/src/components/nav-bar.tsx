1d5c6b5fd3cfa5488581a4e2b769d2adf9513ab6
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2025-06-04 19:25:01 -0700

    [chore] - centered navbar with page

--- frontend/src/components/nav-bar.tsx
+++ frontend/src/components/nav-bar.tsx
@@ -17,7 +17,7 @@ export function NavBar() {
 
   return (
     <>
-      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center items-center">
         <div className="container flex h-16 items-center justify-between px-4">
           {/* Logo */}
           <div className="flex items-center gap-2">
