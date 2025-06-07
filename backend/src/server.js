1087b9c0c1001255593bd7d955f035fa93d2b060
William MacBook <wwilliamw24@gmail.com>
2025-06-06 21:43:28 -0700

    [feat] added rate limiting and slow down to limit requests on the backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,25 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from '../routes/auth.js'
 import postRoutes from '../routes/posts.js'
+import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+});
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 1,
+  delayMs: () => 2000,
+});
 
 // Middleware
 app.use(cors({
@@ -17,15 +31,20 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiter);
+app.use(speedLimiter);
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    objectSrc: ["'none'"],
+    upgradeInsecureRequests: [],
+  }
+}));
 
 // Routes
 app.use('/api/users', authRoutes);
 app.use('/api/posts', postRoutes);
-
-// Test endpoint
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Server is running' });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
