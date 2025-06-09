79b445367e298e2e53e7d4e02a4b762f3ea572f7
Nateroni <nathanpunyataweekul@gmail.com>
2025-06-08 19:07:22 -0700

    [fix] added padding between components in profile card

--- frontend/src/components/profile-modal.tsx
+++ frontend/src/components/profile-modal.tsx
@@ -149,7 +149,7 @@ export function ProfileModal({ isOpen, onClose }: ProfileModalProps) {
                 Your Posts
               </h3>
             </div>
-            <div className="flex-1 min-h-0 overflow-hidden">
+            <div className="flex-1 min-h-0 overflow-hidden pt-4">
               <PostCarousel
                 initialPosts={posts}
                 onLoadMore={handleLoadMore}
